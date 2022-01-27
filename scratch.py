3b32b9e8a3eae92cbbbf26035d4ac6cdfc27c1dc
kyeo95 <55420204+kyeo95@users.noreply.github.com>
2022-01-27 13:51:15 +0900

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -12,13 +12,15 @@ class Packetbnn(nn.Module):
         self.features = nn.Sequential(
 
             BNNConv2d(1, 5, kernel_size=(1,5), stride=1, padding=0, bias=False),
-            #nn.BatchNorm2d(5),
-            #nn.Softsign(),
-
+            #nn.Flatten(),
+            # nn.GroupNorm(1,5),
+            #nn.BatchNorm2d(1),
+            nn.Softsign(),
+            #
             nn.Flatten(),
             #nn.BatchNorm1d(5),
             BNNLinear(5, 1),
-            #nn.BatchNorm1d(num_classes, affine=False),
+            # nn.BatchNorm1d(num_classes, affine=False),
             #nn.LogSoftmax(dim=1),
         )
 
@@ -36,8 +38,11 @@ class Packetbnn(nn.Module):
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.ones_(m.weight)
                 nn.init.zeros_(m.bias)
+            elif isinstance(m, nn.GroupNorm):
+                nn.init.ones_(m.weight)
+                nn.init.zeros_(m.bias)
             elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, 0.5, 0.01)
+                nn.init.uniform_(m.weight, a= 1., b= 1.)
                 nn.init.zeros_(m.bias)
         return
 
@@ -45,36 +50,39 @@ class Packetbnn(nn.Module):
 __all__ = ['BNNLinear', 'BNNConv2d']
 
 
+# class STEFunction(torch.autograd.Function):
+#     @staticmethod
+#     def forward(ctx, input):
+#         return (input-0.5).sign().add_(1).div_(2).float()
+#
+#     @staticmethod
+#     def backward(ctx, grad_output):
+#         return nn.Hardtanh(grad_output)
+#
+# class StraightThroughEstimator(nn.Module):
+#     def __init__(self):
+#         super(StraightThroughEstimator, self).__init__()
+#
+#     def forward(self, x):
+#         x = STEFunction.apply(x)
+#         return x
+
 def Binarize(tensor, quant_mode='det'):
-    if quant_mode == 'det':
-        result = (tensor-0.5).sign().add_(1).div_(2)
-        return result
-    if quant_mode == 'bin':
-        return (tensor >= 0).type(type(tensor)) * 2 - 1
-    else:
-        return tensor.add_(1).div_(2).add_(torch.rand(tensor.size()).add(-0.5)).clamp_(0, 1).round().mul_(2).add_(-1)
+    result = (tensor-0.5).sign().add_(1).div_(2)
+    return result
+
 
 
 class BNNLinear(Linear):
 
     def __init__(self, *kargs, **kwargs):
         super(BNNLinear, self).__init__(*kargs, **kwargs)
-        self.register_buffer('weight_org', self.weight.data.clone())
 
     def forward(self, input):
 
-        #if (input.size(1) != 784) and (input.size(1) != 3072):
-        input.data = Binarize(input.data)
-
-        self.weight.data = Binarize(self.weight_org)
-        out = linear(input, self.weight.data)
-
-        # if not self.bias is None:
-        #     self.bias.org = self.bias.data.clone()
-        #     out += self.bias.view(1, -1).expand_as(out)
+        out = linear(input.data, self.weight)
 
         return out
-
 
 class BNNConv2d(Conv2d):
 
@@ -84,10 +92,10 @@ class BNNConv2d(Conv2d):
 
     def forward(self, input):
 
+        # input.data = Binarize(input.data)
         input.data = Binarize(input.data)
-
+        # self.weight.data = Binarize(self.weight_org)
         self.weight.data = Binarize(self.weight_org)
-
         out = conv2d(input, self.weight.data, None, self.stride,
                      self.padding, self.dilation, self.groups)
 
@@ -108,25 +116,43 @@ losses = []
 #                       [[[1., 0., 0., 0., 0.]]],
 #
 #                       [[[1., 0., 0., 1., 1.]]]])
-input = torch.tensor([[[[0., 0., 1., 1., 0.]]]])
+#input = torch.tensor([[[[0., 0., 1., 1., 0.]]]])
+#input = torch.tensor([[[[0., 1., 1., 1., 1.]]]])
+input = torch.tensor([[[[0., 1., 1., 1., 1.]]]])
 k = Packetbnn()
 k.init_w()
-BNN = BNNConv2d(1,5,(1,5))
-weight = torch.tensor([[[[0.2864, 0.2374, 0.3904, 0.3166, 0.9963]]],
+
+# k = BNNLinear(5,1)
+#weight = torch.tensor([0.2864, 0.2374, 0.3904, 1.3166, 0.9963])
+# with torch.no_grad():
+#     k.weight_org.copy_(weight)
+# print(k.weight)
+# print(k.weight_org)
+# print(Binarize(k.weight))
+# print(Binarize(input))
+# BNN = BNNConv2d(1,5,(1,5))
+# weight = torch.tensor([[[[0.2864, 0.2374, 0.3904, 1.3166, 0.9963]]],
+#
+#
+#         [[[0.8499, 0.6796, 1.0458, 0.6867, 0.1891]]],
+#
+#
+#         [[[0.6125, 0.2220, 1.4095, 0.6262, 0.5675]]],
+#
+#
+#         [[[0.0231, 0.2038, 1.1185, 0.0179, 0.4394]]],
+#
+#
+#         [[[0.1566, 0.7243, 1.7171, 1.3417, 0.6585]]]], requires_grad=True)
+#
 
 
-        [[[0.8499, 0.6796, 0.0458, 0.6867, 0.1891]]],
-
-
-        [[[0.6125, 0.2220, 0.4095, 0.6262, 0.5675]]],
-
-
-        [[[0.0231, 0.2038, 0.1185, 0.0179, 0.4394]]],
-
-
-        [[[0.1566, 0.7243, 0.7171, 0.3417, 0.6585]]]], requires_grad=True)
-
-k.features[0].weight= nn.Parameter(weight)
+# k.features[0].weight_org= nn.Parameter(weight)
+# print(k(input))
+# print(k.features[0].weight_org)
+# print(k.features[0].weight)
+# print(Binarize(k.features[0].weight_org))
+# print(Binarize(k.features[0].weight))
 
 # tensor([[[[0., 0., 0., 0., 1.]]],
 #
@@ -146,8 +172,10 @@ k.features[0].weight= nn.Parameter(weight)
 #target = torch.tensor([[1.], [0.], [0.]])
 target = torch.tensor([[1.]])
 optimizer = torch.optim.Adam(k.parameters(), lr=0.005, weight_decay=1e-5)
+print(k.features[0].weight_org)
+print(k.features[0].weight)
 
-for t in range(1000):
+for t in range(3):
     output = k(input)
     print(output)
     loss = (output-target).pow(2).sum()
@@ -157,6 +185,7 @@ for t in range(1000):
 
     optimizer.zero_grad()
     loss.backward()
+    print(k.features[0].weight.grad)
     for p in k.modules():
         if hasattr(p, 'weight_org'):
             p.weight.data.copy_(p.weight_org)
@@ -164,14 +193,16 @@ for t in range(1000):
     for p in k.modules():
         if hasattr(p, 'weight_org'):
             p.weight_org.data.copy_(p.weight.data.clamp_(-1, 2))
+# print(k.features[0].weight_org)
+# print(k.features[0].weight)
 
 # print(k.features[0].weight)
-print(k.features[0].weight)
-print(k.features[2].weight)
-print(losses[999])
-print(Binarize(k.features[0].weight))
-print(Binarize(k.features[2].weight))
-
+# print(k.features[0].weight)
+# print(k.features[2].weight)
+# # print(losses[999])
+# print(Binarize(k.features[0].weight))
+# print(Binarize(k.features[2].weight))
+# print(torch.cuda.is_available())
 # a = torch.tensor([[[[-0.1980,  0.0351, -0.4719,  0.6720,  1.2105]]]])
 # filter = torch.tensor([[[[-1.0145,  0.0434,  0.1013,  0.2565,  0.1284]]],
 #
